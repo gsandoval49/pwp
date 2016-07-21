6fbf39062661cc0b65735c0f905eb6fa2a5ad3d2
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-21 00:46:51 -0600

    Provided content for persona and user case section

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -25,6 +25,23 @@
 					<p><strong>Purpose:</strong> This project is a site used to present my professional portfolio as an experienced web developer.</p>
 					<p><strong>Audience:</strong> Keeping in mind a professional audience from the I.T. community, this website targets specifically startup founders, CEOs of local businesses, international hiring managers, top recruiters, and my peers in web development.Audience: Keeping in mind a professional audience from the I.T. community, this website targets specifically startup founders, CEOs of local businesses, international hiring managers, top recruiters, and my peers in web development.</p>
 					<p><strong>Goal:</strong> The short term and long term goals of this website is to obtain a full-time/part-time position with a company, appeal to freelance clients, showcase my portfolio to peers and future clients, establish a professional presence within the I.T. community, and present my portfolio as an expert developer for future business endeavors. </p>
+
+					<!-- Persona section here -->
+					<h2>Persona</h2>
+					<p><strong>Name: Nathan Ryan | 33 Years Old</strong></p>
+					<p><strong>Profession:</strong> Owner of a small startup called Dream Team that provides IT services, web development and maintenance to their clients. He is looking to hire a full-time, front end web developer. They have taken on some new projects and require some man power to help assist with their work load. </p>
+					<p><strong>Technology:</strong> Windows 10 user, uses an Asus desktop at the office, an Android Galaxy Note 3 with unlimited 4G connection, and an Asus 17” HD laptop for home.</p>
+					<p><strong>Attitudes and Behaviors:</strong> As an owner, he is consistently busy and can easily put in 12 hour working days. He rises early in the morning and has a calculated social calendar for possible networking and future business opportunities. Nathan is a speed reader. He will not spend more than 30 seconds looking at a potential hire’s resume. If he’s not intrigued or sees qualifying experience within that time frame, he’ll discard. If he is intrigued, he’ll check the candidate’s personal website, the content, portfolio, and previous projects. He’ll spend 2-5 minutes depending on his interest level. David will use social media as a resource. But he prefers referrals and professional references to determine a candidate’s work ethic.</p>
+					<p><strong>Frustration and Needs:</strong> Nathan wants to hire someone with experience, but also openness to assimilate the company’s culture and learn the style of their business. He wants to hire someone who is open to learning a little bit of back-end while working as a front-end developer. He wants to hire someone willing to start at a mid-level salary, but they’ll have decent benefits through the company. With the upcoming workload, Nathan hopes to give an offer letter to someone within 2 weeks. </p>
+					<p><strong>Goals:</strong> Nathan’s priority is to get that ideal qualified candidate on their team within 2.5 weeks. He hopes to deduce much about the candidate through the personal website and their resume. He keeps a checklist nearby and notes the uniqueness of each candidate, while looking at the website. He is willing to make calls to potential candidates, but expects a quick response. He hopes to line up interviews on Thursdays and Fridays after his lunch break. </p>
+
+					<!-- User case section here -->
+					<h2>Use Case</h2>
+					<p>Nathan has received a cover letter and a resume from myself expressing my interest in the web developer position. He’s intrigued by my portfolio on my website and will use social media to see my activity in the I.T. community and our possible associations through LinkedIn and Twitter. </p>
+					<p>Comparing with my LinkedIn profile, he sees my past experience, school, and my references on the resume. He happens to know one of my references, so he will give them a call or text message about me, my competence, work ethic, and personality.</p>
+					<p>It is Monday morning and Nathan plans to make those phone calls and text messages after lunch. He feels up to the task after he’s had a meal. He hopes to have 10 interviews lined up for this Thursday and Friday. He has at least 30 applications to go through in the morning, before he makes those calls between 12:45pm – 2:45pm. He has handed off the priorities for the day to his second in command to have the 7 hours he’s allocated to nothing but this task.</p>
+
+
 				</main>
 		</body>
 </html>
