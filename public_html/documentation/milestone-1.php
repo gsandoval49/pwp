e3507f531fa434b67bfe6a1d55aa9719602279be
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-20 20:52:15 -0600

    deploying test to chrome to ensure text is showing up.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+		<head>
+			<meta charset="utf-8" />
+
+			<!-- Title goes here -->
+			<title>Giles Sandoval Personal Website</title>
+		</head>
+		<body>
+
+				<!-- Place body in container -->
+				<div class="container">
+
+				<!-- Enter header here -->
+				<header>
+				<h1>Giles Sandoval</h1>
+				<h2>Personal Website</h2>
+				</header>
+
+				<main>
+					<p>sample lettering.</p>
+
+				</main>
+		</body>
+</html>
