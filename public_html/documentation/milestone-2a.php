0b17d2832d5942e8b72d2c18dfc3d725febeefda
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-07 17:01:29 -0600

    added html for project

---
+++ public_html/documentation/milestone-2a.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/ >
+
+        <title>Content strategy</title>
+    </head>
+    <!--main body begins-->
+    <body>
+    <header>
+        <h1>
+            Content Strategy
+        </h1>
+        <div class="container">
+            <h2>
+                For Html
+            </h2>
+            <p></p>
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+    </body>
+</html>
