ae093ecd598ab048a52bde542c03276cf56dba95
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-07 17:15:44 -0600

    sections for sitemap and text incorporated

--- public_html/documentation/milestone-2a.php
+++ public_html/documentation/milestone-2a.php
@@ -3,19 +3,26 @@
     <head>
         <meta charset="utf-8"/ >
 
-        <title>Content strategy</title>
+        <title>PWP Milestone 2a</title>
     </head>
     <!--main body begins-->
     <body>
     <header>
         <h1>
-            Content Strategy
+            PWP Milestone 2A
         </h1>
         <div class="container">
             <h2>
-                For Html
+                Sitemap
             </h2>
-            <p></p>
+            <p>This website will consist of 5 sections</p>
+            <ul>
+                <li>Name &  Contact Section</li>
+                <li>About Me</li>
+                <li>Experiences</li>
+                <li>Latest Projects</li>
+                <li>Get in Touch</li>
+            </ul>
         </div>
     </header>
     <main>
