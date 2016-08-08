f6dc6cfa994366e6068aa7ce79f111a1b60eb95a
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-08 17:18:03 -0600

    added a few more statements or corrected bolded sections

--- public_html/documentation/milestone-2a.php
+++ public_html/documentation/milestone-2a.php
@@ -5,15 +5,17 @@
 
         <title>PWP Milestone 2a</title>
     </head>
-    <!--main body begins-->
+
     <body>
     <header>
         <h1>
             PWP Milestone 2A
         </h1>
+    </header>
 
+    <main>
         <!-- Sitemap section -->
-        <div class="container">
+        <div class="container"></div>
         <h2>
             Sitemap
         </h2>
@@ -32,14 +34,13 @@
         <p><strong>Name & Contact Section:</strong> An introduction that says my name, my occupation as a web developer, and my contact me section. My main objective of this website is to get someone to contact me about future projects or a potential job. Most people's eyes automatically center at the middle of the site. At the center of my page they'll see a contact button that takes them to my contact form.</p>
         <p><strong>About Me:</strong> This section will highlight my core skills, and passion for the field of web development. This will showcase my personality and strengths. Next to my about me section will be readable, at a glance sidebar to list my core skills. I will invite them to work with me or hire me as a web developer.</p>
         <p><strong>Experiences:</strong> My past experience will display my professional history, a short resume and showcase my skills as a web developer and that I am a team player.</p>
-        <p><strong>Latest Projects</strong> This will house my latest projects with web development and media. It will responsive and continue to house future projects after graduating from Deep Dive Coding Bootcamp.</p>
-        <p><strong>Get in touch section will have the final invitation to reach out for a future project or potential job. I will also house the contact me form section that sends to my email.</strong> </p>
+        <p><strong>Latest Projects:</strong> This will house my latest projects with web development and media. It will responsive and continue to house future projects after graduating from Deep Dive Coding Bootcamp. It will house work from future clients, this will most likely be inactive until I get future clients</p>
+        <p><strong>Get in touch:</strong> This section will have the final invitation to reach out for a future project or potential job. I will also house the contact me form section that sends to my email.</p>
 
-
-
-        </div>
-    </header>
-    <main>
+        <!-- Reactive wireframe using html, bootstrap and little bit of css -->
+        <div class="container"</div>
+        <h2>Responsive Wireframe Link</h2>
+        <a href="https://bootcamp-coders.cnm.edu/~gsandoval49/wireframe-sandbox/index.php">Link to my responsive wireframe.</a>
 
     </main>
     <footer>
