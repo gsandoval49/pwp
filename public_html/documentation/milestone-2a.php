f76b335dcc4cfa697483da3d7e0b48aefcc56d9e
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-07 18:00:24 -0600

    content strategy section created

--- public_html/documentation/milestone-2a.php
+++ public_html/documentation/milestone-2a.php
@@ -11,18 +11,32 @@
         <h1>
             PWP Milestone 2A
         </h1>
+
+        <!-- Sitemap section -->
         <div class="container">
-            <h2>
-                Sitemap
-            </h2>
-            <p>This website will consist of 5 sections</p>
-            <ul>
-                <li>Name &  Contact Section</li>
-                <li>About Me</li>
-                <li>Experiences</li>
-                <li>Latest Projects</li>
-                <li>Get in Touch</li>
-            </ul>
+        <h2>
+            Sitemap
+        </h2>
+        <p>This website will consist of 5 sections...</p>
+        <ol>
+            <li>Name &  Contact Section</li>
+            <li>About Me</li>
+            <li>Experiences</li>
+            <li>Latest Projects</li>
+            <li>Get in Touch</li>
+        </ol>
+
+        <!-- Content strategy section -->
+        <div class="container"</div>
+        <h2>Content Strategy</h2>
+        <p><strong>Name & Contact Section:</strong> An introduction that says my name, my occupation as a web developer, and my contact me section. My main objective of this website is to get someone to contact me about future projects or a potential job. Most people's eyes automatically center at the middle of the site. At the center of my page they'll see a contact button that takes them to my contact form.</p>
+        <p><strong>About Me:</strong> This section will highlight my core skills, and passion for the field of web development. This will showcase my personality and strengths. Next to my about me section will be readable, at a glance sidebar to list my core skills. I will invite them to work with me or hire me as a web developer.</p>
+        <p><strong>Experiences:</strong> My past experience will display my professional history, a short resume and showcase my skills as a web developer and that I am a team player.</p>
+        <p><strong>Latest Projects</strong> This will house my latest projects with web development and media. It will responsive and continue to house future projects after graduating from Deep Dive Coding Bootcamp.</p>
+        <p><strong>Get in touch section will have the final invitation to reach out for a future project or potential job. I will also house the contact me form section that sends to my email.</strong> </p>
+
+
+
         </div>
     </header>
     <main>
