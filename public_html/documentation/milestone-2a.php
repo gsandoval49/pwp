39be0d656d63184367c01e86b85a37bfe3fa7dc8
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-27 16:07:30 -0600

    head utils added and created js file.

--- public_html/documentation/milestone-2a.php
+++ public_html/documentation/milestone-2a.php
@@ -1,9 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8"/ >
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>PWP Milestone 2a</title>
+        <!-- Latest compiled and minified CSS-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- OPTIONAL THEME HERE -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- ADD YOUR CUSTOM CSS HERE -->
+        <link rel="stylesheet" href="../css/style.css" type="text/css">
+
+        <!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+        <!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+        <!-- TITLE HERE-->
+        <title>Giles Sandoval</title>
     </head>
 
     <body>
