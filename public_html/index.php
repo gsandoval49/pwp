985999df47896ec4287d92f61efdd8c8fa1ba835
Giles Sandoval <gsandoval2277@gmail.com>
2016-09-04 23:18:10 -0600

    added soundboard to social section

--- public_html/index.php
+++ public_html/index.php
@@ -159,7 +159,7 @@
 					  <div class="row">
 						  <div class="col-xs-12 col-md-6">SOCIAL ICONS HERE</div>
 						  <div class="col-xs-12 col-md-6">PICTURE HERE
-							  <div class="about-photo"></div>
+							  <div class="social-photo"></div>
 						  </div>
 					  </div>
 				  </div>
