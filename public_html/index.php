e68c5159be54f66dd0ad0eef08bc33d278f12321
Giles Sandoval <gsandoval2277@gmail.com>
2016-09-09 08:38:55 -0600

    took out stellar scripts because i didn't need and giving me errors in browser. No longer using.

--- public_html/index.php
+++ public_html/index.php
@@ -54,9 +54,6 @@
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-		<!-- loop over $ADD_SCRIPT array to output additional <script> tags, if applicable. -->
-		<script src="js/jquery.stellar.min.js" type="text/javascript"/></script>
-		<script src = "js/stellar-parallax.js" type = "text/javascript" / ></script>
 
 		<!--for recaptcha-->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
